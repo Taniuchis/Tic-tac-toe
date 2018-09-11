8f1f56c09eacc6e1eeee42a0d5a77365333933a5
Tania Munoz <tania.m.jimenez24@gmail.com>
2018-09-11 11:57:09 -0500

    empty variables for the board

--- tictactoe.rb
+++ tictactoe.rb
@@ -1,1 +1,20 @@
 #tictactoe
+
+def  empty_board
+   @x1 = " "
+   @x2 = " "
+   @x3 = " "
+   @y1 = " "
+   @y2 = " "
+   @y3 = " "
+   @z1 = " "
+   @z2 = " "
+   @z3 = " "
+end
+
+def start
+  puts "Tic Tac Toe game"
+  puts "Please you need to write the coordinates"
+end
+
+start
