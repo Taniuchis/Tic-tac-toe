174655e705ede0242150c973e10d120423c6c438
Tania Munoz <tania.m.jimenez24@gmail.com>
2018-09-12 13:16:52 -0500

    size board n*n

--- tictactoe.rb
+++ tictactoe.rb
@@ -1,61 +1,37 @@
 
 #tictactoe
-
-def  empty_board
-   @x1 = " "
-   @x2 = " "
-   @x3 = " "
-   @y1 = " "
-   @y2 = " "
-   @y3 = " "
-   @z1 = " "
-   @z2 = " "
-   @z3 = " "
+def coordinates
+   
 end
 
-def print_board
-  puts
-  puts "   1   2   3"
-  puts "X  #{@x1} | #{@x2} | #{@x3} " 
-  puts "  ---|---|---"
-  puts "Y  #{@y1} | #{@y2} | #{@y3} "
-  puts "  ---|---|---"
-  puts "Z  #{@z1} | #{@z2} | #{@z3} "
-  puts
-  
-end
 def player
-  for i in 1..9
+  plays = @board_size * @board_size
+  for i in 1..plays
     if i % 2 == 0 
       puts "player 2"
       puts "Please you need to write the coordinates"
       coor = gets.chomp #var that save the coordinate that user choice
       puts coor
-      player = "Y"
-      turn(coor, player)
+      player = "O"
+      #turn(coor, player)
     else
       puts "player 1"
       puts "Please you need to write the coordinates"
       coor = gets.chomp #var that save the coordinate that user choice
-      puts coor
       player = "X"
-      turn(coor, player)
+      #turn(coor, player)
     end  
+  end
+
+  puts @board[0][0]
+  if coordinates == true
+    puts "existen las cord"
+  else
+    puts "no existen"
   end
 end
 
-
 def turn (coor , player)
-  coor_hash = {"x1" => @x1,
-               "x2" => @x2,
-               "x3" => @x3,
-               "y1" => @y1,
-               "y2" => @y2,
-               "y3" => @y3,
-               "z1" => @z1,
-               "z2" => @z2,
-               "z3" => @z3}
-
     coor_hash.each do |choice, square|
       if choice == coor
           square.sub!(" ", player)
@@ -65,12 +41,53 @@ def turn (coor , player)
  print_board
 end
 
-def check_win
-  
+
+
+def  empty_board
+  @board = []
+  # llenar tablero
+  @board_size.times do
+    column = []
+    @board_size.times do
+      column.push ' '
+    end
+    @board.push column
 end
+ 
+
+end
+
+def print_board
+  for i in 0..@board_size-1
+    coord = i
+    coord = coord.to_s
+    print "  " + coord
+  end
+  puts
+  i = 0 
+  # imprimir matriz
+  @board.each do |column|
+    coord = i.to_s
+    print coord + " "
+    i= i+1
+    column.each do |card|
+      #card.sub!(" ", "X")
+      print card
+      print '  ' # print no imprime un salto de linea al final del output
+    end
+    puts
+    #puts "  --|--|--"
+  end
+  
+
+end
+
+
 
 def start
   puts "Tic Tac Toe game"
+  puts "Choice the size of the board"
+  @board_size = gets.chomp.to_i
   empty_board
   print_board
   player
